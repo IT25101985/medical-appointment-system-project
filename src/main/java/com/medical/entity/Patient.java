c6a806144427b770386c0ff4d84a12c6598959d9
IT25101985 <it25101985@my.sliit.lk>
2026-05-08 02:08:12 +0530

    update

---
+++ src/main/java/com/medical/entity/Patient.java
@@ -0,0 +1,26 @@
+package com.medical.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("PATIENT")
+public class Patient extends User {
+
+    private String bloodGroup;
+
+    public Patient() {
+        super();
+        this.setRole("ROLE_PATIENT");
+    }
+
+    // Constructor demonstrating Inheritance
+    public Patient(String username, String password, String fullName, String bloodGroup) {
+        super(username, password, fullName);
+        this.bloodGroup = bloodGroup;
+        this.setRole("ROLE_PATIENT");
+    }
+
+    public String getBloodGroup() { return bloodGroup; }
+    public void setBloodGroup(String bloodGroup) { this.bloodGroup = bloodGroup; }
+}

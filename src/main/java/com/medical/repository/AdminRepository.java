561b5796eb8373a9fcd78f7a003175d6f3e57c61
IT25101985 <it25101985@my.sliit.lk>
2026-05-16 22:54:52 +0530

    update code adminReository

--- src/main/java/com/medical/repository/AdminRepository.java
+++ src/main/java/com/medical/repository/AdminRepository.java
@@ -1,4 +1,9 @@
 package com.medical.repository;
 
-public class AdminRepository {
+import com.medical.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
 }

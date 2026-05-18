c3b348edf6c72db8a1ed1f47d5b661c93361d67e
IT25101989 <it25101989@my.sliit.lk>
2026-05-18 18:58:02 +0530

    Adding patient fields in Patient entity

--- src/main/java/com/medical/entity/Patient.java
+++ src/main/java/com/medical/entity/Patient.java
@@ -4,5 +4,8 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Patient extends User {
-
+    private String bloodGroup;
+    private String bloodPressure;
+    private String heartRate;
+    private String emergencyContact;
 }

05be219ab291450c9f86fa422490011be941d19a
IT25101989 <it25101989@my.sliit.lk>
2026-05-18 19:53:37 +0530

    Configuring JPA discriminator value

--- src/main/java/com/medical/entity/Patient.java
+++ src/main/java/com/medical/entity/Patient.java
@@ -1,8 +1,10 @@
 package com.medical.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("PATIENT")
 public class Patient extends User {
     private String bloodGroup;
     private String bloodPressure;

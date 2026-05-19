35560359058f6662734e36d265be4a8f7ba50117
IT25101989 <IT25101989@my.sliit.lk>
2026-05-19 21:35:26 +0530

    Implementing PatientProfileController with dependencies

---
+++ src/main/java/com/medical/controller/PatientProfileController.java
@@ -0,0 +1,24 @@
+package com.medical.controller;
+
+import com.medical.entity.Patient;
+import com.medical.entity.User;
+import com.medical.repository.UserRepository;
+import com.medical.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/patient")
+public class PatientProfileController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+}

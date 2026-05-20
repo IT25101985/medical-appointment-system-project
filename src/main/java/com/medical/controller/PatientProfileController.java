27aa1dcc93acaddb1b630fc746d576ac9968e033
IT25101989 <IT25101989@my.sliit.lk>
2026-05-20 15:43:25 +0530

    Adding Doctor,Appointment,Invoice and MedicalRecord dependancies in PatientProfileController

--- src/main/java/com/medical/controller/PatientProfileController.java
+++ src/main/java/com/medical/controller/PatientProfileController.java
@@ -14,6 +14,7 @@ import com.medical.service.InvoiceService;
 import com.medical.service.DoctorService;
 import com.medical.repository.PatientRepository;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;

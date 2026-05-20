ea75e4b97247353e0c4035b3246bd83300d71df2
IT25101989 <IT25101989@my.sliit.lk>
2026-05-20 16:14:36 +0530

    Adding Spring web and Autowired imports to PatientProfileController

--- src/main/java/com/medical/controller/PatientProfileController.java
+++ src/main/java/com/medical/controller/PatientProfileController.java
@@ -14,9 +14,9 @@ import com.medical.service.InvoiceService;
 import com.medical.service.DoctorService;
 import com.medical.repository.PatientRepository;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;

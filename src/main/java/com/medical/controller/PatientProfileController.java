3fd45f06fd93f343c29c0ff29f40281ebceaf690
IT25101989 <IT25101989@my.sliit.lk>
2026-05-20 16:19:49 +0530

    Importing java.util collections and java.security utilities

--- src/main/java/com/medical/controller/PatientProfileController.java
+++ src/main/java/com/medical/controller/PatientProfileController.java
@@ -20,6 +20,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller

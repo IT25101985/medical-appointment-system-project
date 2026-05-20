4771e02616fbae7e405124b08f21dc7493c74391
IT25101989 <IT25101989@my.sliit.lk>
2026-05-20 19:26:46 +0530

    adding all files from my device

---
+++ src/test/java/com/medical/medicalappointmentsystemproject/MedicalAppointmentSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.medicalappointmentsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentSystemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

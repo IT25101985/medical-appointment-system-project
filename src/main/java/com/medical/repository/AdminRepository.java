41dacdc18835ce893d3d91156286a447a145a984
IT25102003 <IT25102003@my.sliit.lk>
2026-05-21 12:56:54 +0530

    remove extra file admin

--- src/main/java/com/medical/repository/AdminRepository.java
+++ src/main/java/com/medical/repository/AdminRepository.java
@@ -1,6 +1,5 @@
 package com.medical.repository;
 
-import com.medical.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

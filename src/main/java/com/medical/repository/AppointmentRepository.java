e0eb4e15671a37210b7dde64b1ff229dbbc3c980
IT25102003 <IT25102003@my.sliit.lk>
2026-05-21 15:09:00 +0530

    AppointmentRepository update

--- src/main/java/com/medical/repository/AppointmentRepository.java
+++ src/main/java/com/medical/repository/AppointmentRepository.java
@@ -1,6 +1,8 @@
 package com.medical.repository;
 
 import com.medical.entity.Appointment;
+import com.medical.entity.Doctor;
+import com.medical.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

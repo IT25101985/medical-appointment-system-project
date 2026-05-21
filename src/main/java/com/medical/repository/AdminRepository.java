dff2b34f832525a0e4b5422620c912c1f4b4cbee
IT25102003 <IT25102003@my.sliit.lk>
2026-05-21 14:05:19 +0530

    admin repository file remove

--- src/main/java/com/medical/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.medical.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
